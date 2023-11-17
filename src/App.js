8e7a9f89f090528373e5254a89db6511924f856f
Jesse Granier <jesse.granier@lapiscine.pro>
2023-11-17 16:27:51 +0100

    voilà

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './pages/Home';
-import Pokemons from './pages/Pokemons';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import HomePage from "./page/HomePage";
+import PokemonsPage from "./page/PokemonsPage";
+import PokemonDetailPage from "./page/PokemonDetailPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />     
-        <Route path="/pokemons" element={<Pokemons />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/pokemons" element={<PokemonsPage />} />
+        <Route path="/pokemons/:pokemonId/details" element={<PokemonDetailPage />} />
       </Routes>
     </BrowserRouter>
   );
